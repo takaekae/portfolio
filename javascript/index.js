877f3ce8c78c14ea04841ea10a6dca551a2ea222
Pudding(Kiyo) <95976269+Kiyo-18@users.noreply.github.com>
2022-12-07 00:24:01 +0800

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,5 @@
 var loader  = document.getElementById('loader');
-var remove = document.getElementById('container');
 
 window.addEventListener("load", function(){
         loader.style.display = "none";
-        remove.style.display = "none";
 });
